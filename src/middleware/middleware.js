bfc0746d1d336d32604227318b584a55df381324
SAI RATHOD <101806433+saiprasad8955@users.noreply.github.com>
2022-06-27 18:14:39 +0530

    AUTHENTICATION

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -13,11 +13,16 @@ const authentication = async function(req,res,next){
     //If no token is present in the request header return error
     if (!token) return res.status(401).send({ status: false, msg: "Token must be present" });
 
-    //if token is present then decode the token
-    let decodedToken = jwt.verify(token,"Book-Management")
-    
-    // Check Decoded token is here or not
-    if(!decodedToken) return res.status(401).send({ status : false, msg : "Token is Not Present"})
+    // Callback function for decodedtoken 
+    let token = function (err, token) {
+        if (err) {
+          return res.status(400).send({status:false, msg:"Invalid Token!!!!"});
+        }
+        else { return token }
+      }
+  
+      //if token is present then decode the token
+      let decodedToken = jwt.verify(token, "Book-Management",token)
 
     req.decodedToken = decodedToken 
     // if Everything is ok then we head towards Api's
